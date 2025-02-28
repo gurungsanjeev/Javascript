63744d59f95153d52cf73b50ed08c96140b58924
sanjeev gurung <gurungsanjeev91@gmail.com>
2025-02-28 16:58:16 +0545

    init

---
+++ Day1_Basis/script.js
@@ -0,0 +1,37 @@
+// // let a = null;
+// // let stu ={
+// //     name: "Sanjeev",
+// //     age : 16,
+// //     isStudent: true
+// // }
+
+// // console.log(stu.name);
+
+
+
+// /* Using the concept of the filter method */
+// let num =[1,2,3,4,5,6,7,8,9];
+// let evenNum= num.filter(num=>num%2===0)
+
+// console.log(evenNum);
+
+
+// /* Using the concept of the reduce method */
+// let number = [1,2,3,4,5];
+// let redu= number.reduce((accu, numbers)=>
+//     accu + numbers
+// )
+// console.log(redu);
+
+
+
+
+const sub=(name)=>{
+    // value= document.getElementById("txt").value;
+    // alert("Welcome " + value + " to our workspace");
+    // console.log("Welcome " + value + " to our workspace");
+
+    alert(`"Welcome ${name}, to our workspace!!"`)
+}
+sub("sanjeev")
+
