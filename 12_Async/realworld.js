7bed10a5b6dc2182ecf8ef4965aa436b45b74146
sanjeev gurung <gurungsanjeev91@gmail.com>
2025-03-07 11:21:26 +0545

    added

--- 12_Async/realworld.js
+++ 12_Async/realworld.js
@@ -1,19 +1,27 @@
-async function Example(){
-    try{
-
-        let response = await fetch("https://jsonplaceholder.typicode.com/posts")
+async function Example() {
+    try {
+        let response = await fetch("https://jsonplaceholder.typicode.com/posts");
         let value = await response.json();
         
+        let output = document.getElementById("container"); // Get the container div
+        output.innerHTML = ""; // Clear previous data
 
-        value.map((post)=>{
-            console.log(post.title);
-        })
+        value.forEach((post) => { // Loop through all posts
+            let postElement = document.createElement("div"); // Create a new div
+            postElement.innerHTML = `
+            <div style="border:1px solid black padding:10px width:50%">
+                <h3>${post.title}</h3>
+                <hr>
+                <p>${post.body}</p>
+                
+                </div>
+            `;
+            output.appendChild(postElement); // Append to container
+        });
 
-
-
-    }catch(err){
-        console.log("Error aayo hai!")
+    } catch (err) {
+        console.log("Error aayo hai!", err);
     }
-
 }
+
 Example();
